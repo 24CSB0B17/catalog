bf5b55da6c1d96fd6640c90ffbfaed597888c2ce
24CSB0B17 <dm24csb0b17@student.nitw.ac.in>
2025-03-09 23:07:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 class Product {
-  constructor(id, name, price, image) {
+  constructor(id, name, price, image, description) {
     this.id = id;
     this.name = name;
     this.price = price;
     this.image = image;
+    this.description = description;
   }
 }
 
@@ -33,8 +34,10 @@ class Cart {
   }
 }
 
-const product1 = new Product(1, 'Product 1', 19.99, 'product1.jpg');
-const product2 = new Product(2, 'Product 2', 29.99, 'product2.jpg');
+const product1 = new Product(1, 'Product 1', 19.99, 'product1.jpg', 'This is an amazing product that you will love!');
+const product2 = new Product(2, 'Product 2', 29.99, 'product2.jpg', 'A premium product with excellent features.');
+const product3 = new Product(3, 'Product 3', 39.99, 'product3.jpg', 'Best for everyday use, high durability and performance.');
+const product4 = new Product(4, 'Product 4', 49.99, 'product4.jpg', 'An innovative product that makes life easier.');
 
 const cart = new Cart();
 
@@ -45,6 +48,10 @@ document.querySelectorAll('.add-to-cart').forEach(button => {
       cart.addItem(product1);
     } else if (productId === 'product2') {
       cart.addItem(product2);
+    } else if (productId === 'product3') {
+      cart.addItem(product3);
+    } else if (productId === 'product4') {
+      cart.addItem(product4);
     }
   });
 });
